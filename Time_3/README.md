ade5b1b54a9c53e9fc2c69605083b57b324bb5a4
Luiz Coura <luizcoura@gmail.com>
2018-09-14 03:20:30 -0300

    instalação do hyperledger com vagrant

--- Time_3/README.md
+++ Time_3/README.md
@@ -1,3 +1,21 @@
 # STAGIHO-TR
 
 **S**oluções **T**ecnológicas **A**plicáveis ao **G**erenciamento de **I**nformações **H**ospitalares **O**stensivas com **T**empo **R**eal
+
+Criando a business network:
+yo hyperledger-composer:businessnetwork
+cd tutorial-network/
+composer archive create -t dir -n .
+composer network install --card PeerAdmin@hlfv1 --archiveFile tutorial-network@0.0.1.bna
+composer network start --networkName tutorial-network --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer card import --file networkadmin.card
+composer network ping --card admin@tutorial-network
+
+Listando:
+composer card list
+composer network list -c admin@tutorial-network
+
+Iniciando REST Server:
+composer-rest-server -c admin@tutorial-network -n always -w true
+
+
---
+++ Time_3/US42.d/install-hyperledger-composer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# https://hyperledger.github.io/composer/latest/installing/development-tools.html
+
+export FABRIC_VERSION=hlfv12
+
+sudo npm config set user 0
+sudo npm config set unsafe-perm true
+
+# Essential CLI tools
+sudo npm install -g composer-cli@0.20
+
+# Utility for running a REST Server on your machine to expose your business networks as RESTful APIs
+sudo npm install -g composer-rest-server@0.20
+
+# Useful utility for generating application assets
+sudo npm install -g generator-hyperledger-composer@0.20
+
+# Yeoman is a tool for generating applications, which utilises generator-hyperledger-composer:
+sudo npm install -g yo
+
+# Browser app for simple editing and testing Business Networks:
+sudo npm install -g composer-playground@0.20
+
+# Fix permission
+sudo chown -R vagrant:vagrant .config/
+
+# In a directory of your choice (we will assume ~/fabric-dev-servers), get the .tar.gz file that contains the tools to install Hyperledger Fabric:
+mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
+curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
+tar -xvf fabric-dev-servers.tar.gz
+
+# Use the scripts you just downloaded and extracted to download a local Hyperledger Fabric v1.2 runtime:
+cd ~/fabric-dev-servers
+./downloadFabric.sh
