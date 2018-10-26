3f2bf4d5082bf3fb8da9e0c36d9a1406a8390d8c
lewisferreira <luizflrf@gmail.com>
2018-10-26 00:14:36 -0400

    adicionando nodeJS US45

---
+++ Time_3/US45/scripts/mainUS45.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/*
+ * export KAFKA="192.168.33.10" 
+ * node main.js
+ */
+
+var kafka = require('kafka-node');
+var request = require('request-promise');
+
+const client = new kafka.Client(process.env.KAFKA || 'localhost'); // alterar endereço do Kafka conforme seu ambiente
+const consumer = new kafka.Consumer(client, [{ topic: 'exoRasp', fromOffset: -1}]);
+
+consumer.on('error', function (err) {
+    console.log(err);
+})
+
+consumer.on('message', function (message) {
+    console.log(message.value);
+
+    request({
+        method: 'POST',
+        uri: 'http://35.232.70.182:3000/api/stagihotr.paciente.MovimentoParticipant',
+        body: JSON.parse(message.value),
+        json: true
+    }).then(function (body) {
+        console.log(body);
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
