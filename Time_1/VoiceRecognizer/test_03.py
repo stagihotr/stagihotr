ab73abe9c0cc0ffffa315169a58ebc5ef96b1cb2
fkfouri <kfouri.fabio@gmail.com>
2018-11-16 16:00:19 -0200

    Teste de reconhecimento de voz

--- Time_1/VoiceRecognizer/test_03.py
+++ Time_1/VoiceRecognizer/test_03.py
@@ -3,10 +3,10 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 #print(sr.Microphone.list_microphone_names())
-#mic = sr.Microphone(device_index=2)
+mic = sr.Microphone(device_index=3)
 
 
-mic = sr.Microphone()
+#mic = sr.Microphone()
 
 with mic as source:
     #r.adjust_for_ambient_noise(source)
