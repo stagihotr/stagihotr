05102e5d0df8b8fbdcae05718e912c708a40c368
fkfouri <kfouri.fabio@gmail.com>
2018-11-20 23:15:17 -0200

    Envio de mensagens de Voz para o Kafka

--- Time_1/VoiceRecognizer/Kafka_Producer.py
+++ Time_1/VoiceRecognizer/Kafka_Producer.py
@@ -13,9 +13,9 @@ KAFKA_BROKERS = '35.202.217.21:9092' #'192.168.100.100:9091' # see step 1
 producer = KafkaProducer(bootstrap_servers=KAFKA_BROKERS)
 
 # Must send bytes
-messages = [b'STGHTRTOP002PNL004EXO008LF50450RN1542443892d47531b30443453944f2528da721f835_']
+messages = [b'STGHTRTOP002PNL004EXO008LF50450RN1542443892d47531b30443453944f2528da721f835']
  
 # Send the messages
 for m in messages:
-    producer.send(KAFKA_TOPIC, m)
+    producer.send(KAFKA_TOPIC, m).get(timeout=30)
 
